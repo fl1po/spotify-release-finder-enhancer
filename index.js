3a6aa0a2dbd2d2ae890c8e916c0dda84eb7e4551
iomitryako <iomitryako@mts.ru>
2022-05-28 17:58:27 +0300

    added genres exclusions with local storage

--- index.js
+++ index.js
@@ -8,18 +8,25 @@ function getData() {
         }));
     }
     var { artists, genres } = storageData;
-    return { artists, genres, storageData };
+    return { artists, genres, storageData, storageKey };
 }
 
-var genres = document.getElementsByClassName('genrename');
+var genreTags = [...document.getElementsByClassName('genrename')];
 var artistTags = [...document.getElementsByTagName("b")];
 
 function removeNodes(nodes) {
     nodes.forEach((tag) => tag.parentElement.parentElement.style.display = 'none');
 }
 
+function removeNextNodes(nodes) {
+    nodes.forEach((tag) => {
+        tag.nextElementSibling.style.display = 'none';
+        tag.style.display = 'none';
+    });
+}
+
 function excludeArtist(artist) {
-    const { storageData, artists } = getData();
+    const { storageData, artists, storageKey } = getData();
     const newData = {
         ...storageData,
         artists: [...artists, artist]
@@ -33,15 +40,30 @@ function excludeArtists() {
     removeNodes(artistTags.filter((tag) => artists.includes(tag.innerText)));
 }
 
-function appendRemoveNode(tag, className) {
+function excludeGenre(genre) {
+    const { storageData, genres, storageKey } = getData();
+    const newData = {
+        ...storageData,
+        genres: [...genres, genre]
+    };
+    localStorage.setItem(storageKey, JSON.stringify(newData));
+    removeNextNodes(genreTags.filter((tag) => tag.children[0].innerText === genre));
+}
+
+function excludeGenres() {
+    const { genres } = getData();
+    removeNextNodes(genreTags.filter((tag) => genres.includes(tag.children[0].innerText)));
+}
+
+function appendRemoveNode(node, className, exclude) {
     const removeNode = document.createElement('button');
     removeNode.onclick = (e) => {
         e.preventDefault();
-        excludeArtist(tag.innerText);
+        exclude(node.children[0].innerText);
     }
     removeNode.className = className;
     applyRemoveNodeStyles(removeNode);
-    tag.parentElement.append(removeNode);
+    node.append(removeNode);
 }
 
 function setStyle(node, styles) {
@@ -62,18 +84,28 @@ function applyRemoveNodeStyles(removeNode) {
     setStyle(removeNode, defaultStyles);
 }
 
-function setData() {
-    artistTags.forEach((tag, i) => {
-        const className = tag.innerText + i;
-        const removeNode = tag.parentElement.getElementsByClassName(className)[0];
+function getClassName(node, i) {
+    return node.children[0].innerText + i;
+}
+
+function setRemoveNodes(tags, isGenre) {
+    tags.forEach((tag, i) => {
+        const node = isGenre ? tag : tag.parentElement;
+        const className = getClassName(node, i);
+        const removeNode = node.getElementsByClassName(className)[0];
         if (!removeNode) {
-            appendRemoveNode(tag, className);
+            appendRemoveNode(node, className, isGenre ? excludeGenre : excludeArtist);
         } else {
             applyRemoveNodeStyles(removeNode);
         };
-        
     });
+}
+
+function setData() {
+    setRemoveNodes(artistTags);
     excludeArtists();
+    setRemoveNodes(genreTags, true);
+    excludeGenres();
 }
 
 setData();
